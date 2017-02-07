ec8a4234c107101879c0433fd51c556463bd250d
akashadhikari <akash_532998@yahoo.com>
2017-02-07 20:22:24 +0545

    first

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="#">SwaziStore</a> &copy; 2017-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Version %s';
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/demo/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/demo/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/demo/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/demo/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/demo/upload/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/demo/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/demo/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/demo/upload/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/demo/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/demo/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/demo/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/demo/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/demo/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
